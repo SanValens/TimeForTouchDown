77c70eecf0fff9c08a3af8328b1c9b691eec5f42
Santiago Valenzuela <sa_valens@hotmail.com>
2024-01-07 22:10:47 -0500

    Comentarios agregados para mejor entendimiento del programa

--- time_for_touchdown.py
+++ time_for_touchdown.py
@@ -1,39 +1,39 @@
 import matplotlib.pyplot as plt
 
-LAMBDA = 0.81 * 0.007853982 / 2
+LAMBDA = 0.81 * 0.007853982 / 2 #Valores constantes organizados como un valor unico
 MASS = 0.6
 WEIGHT = MASS * 9.8
-valores_variable = []
+
+#Arreglos para acumular datos de la gráfica
+valores_eje_y = []
 iteraciones_lista = []
+
 vel = 0
 time = 0
 net_force = 0
 friction_force = 0
 distance = 0
 altitude_init = 2600
-delta_time = 0.0001
-density = -(0.00009824 * altitude_init) + 1.2027
-counter = 1
+delta_time = 0.00001 #"Paso" en segundos que va a dar el programa entre una iteración y otra
 
 while (altitude_init-distance > 2150):
-    density = -0.00009824*(altitude_init-distance) + 1.2027
-    friction_force = (vel * vel) * LAMBDA * density
-    vel += ((WEIGHT - friction_force) / MASS) * delta_time
-    distance += vel * delta_time
+    density = -0.00009824*(altitude_init-distance) + 1.2027 #Ecuación que calcula la densidad con respecto a la altura
+    friction_force = (vel * vel) * LAMBDA * density #Calcula la fuerza de fricción
+    vel += ((WEIGHT - friction_force) / MASS) * delta_time #Calcula la velocidad
+    distance += vel * delta_time #Calcula la distancia que ha recorrido desde donde se suelta el satélite
     time += delta_time  
-    valores_variable.append(vel)
-    iteraciones_lista.append(counter)
-    counter+=1
+    valores_eje_y.append(vel)
+    iteraciones_lista.append(time)
 
 print()
-print(f'Tiempo para tocar el suelo: {time} segundos')
+print(f'Tiempo que tarda para tocar el suelo: {time} segundos')
 print()
 print(f'Distancia recorrida: {distance} metros')
 print()
 print(f'Velocidad antes de tocar el suelo: {vel} m/s')
 print()
 
-plt.plot(iteraciones_lista, valores_variable, marker='o')
+plt.plot(iteraciones_lista, valores_eje_y, marker='o')
 plt.title('Grafico de la velocidad respecto al tiempo')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Velocidad (m/s)')
